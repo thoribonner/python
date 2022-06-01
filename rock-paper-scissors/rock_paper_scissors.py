6cbe1c37934dc05ba5ea2f6abccb7188e20593ab
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-06-01 02:59:49 -0400

    rock paper scissors

---
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -0,0 +1,65 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+nothing = '''
+      _
+     / )
+    / /
+   ( (  __   _
+   (\ \( .> /_)
+   (\\\ \ \_/ /
+    \       /
+     \    _/
+     /   /
+    /   /
+   /   /
+'''
+
+#Write your code below this line 👇
+options = [rock, paper, scissors, nothing]
+
+computer = random.randint(0, 2)
+
+player = int(input("What do you choose?\nType 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if player > 3 or player < 0:
+	player = 3
+	
+outcome = ""
+
+if player == computer:
+	outcome = "draw"
+elif (player == 0 and computer == 2) or (player > computer and computer != 0 and player < 3 and player > 0):
+	outcome = "WOOHOO\nYOU WON"
+else:
+	outcome = "whomp whomp\nyou lose"
+	
+print(options[player])
+print(f"Computer chose:\n {options[computer]}")
+print(outcome)
